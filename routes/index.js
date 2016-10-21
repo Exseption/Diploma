167fe8f618f61948492d27da8845611e6b725b38
brainsurgery <brain5ur9ery@gmail.com>
2016-10-21 20:29:46 +0900

    отлично!

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Главная', longTitle: 'Очень длинный заголовок' });
 });
 
 module.exports = router;
