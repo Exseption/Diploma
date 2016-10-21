9f0fc7255ab01556e2a5977122ba9c0564111b82
brainsurgery <brain5ur9ery@gmail.com>
2016-10-21 20:00:07 +0900

    нормалёк

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var routes = require('./routes/index');
 var seminars = require('./routes/seminars');
+var about = require('./routes/about');
+var signup = require('./routes/signup');
+var signin = require('./routes/signin');
+
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/seminars', seminars);
+app.use('/about', about);
+app.use('/signup', signup);
+app.use('/signin', signin);
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
