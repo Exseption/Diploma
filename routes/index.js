c883c7ba42a95844bc49f72c5ac3081220995ac6
brainsurgery <brain5ur9ery@gmail.com>
2016-10-26 08:20:37 +0900

    upd

--- routes/index.js
+++ routes/index.js
@@ -38,7 +38,6 @@ module.exports = function (passport) {
         res.render('index', {
             title: 'Главная',
             longTitle: 'Очень длинный заголовок',
-            portalTitle: 'Название',
             questionTitle: 'Название вопроса',
             questionBody: 'Очень много текста',
             numberOfAnswers: '4'
@@ -48,7 +47,6 @@ module.exports = function (passport) {
     router.get('/about', function(req, res, next) {
         res.render('about', {
             title: 'О нас'
-            , portalTitle: 'Название'
             , aboutBody: 'Очень много текста'
         });
     });
@@ -56,28 +54,24 @@ module.exports = function (passport) {
     router.get('/login', function(req, res, next) {
         res.render('login', {
             title: 'Войти',
-            portalTitle: 'Название'
         });
     });
 
     router.get('/register', function(req, res, next) {
         res.render('register', {
             title: 'Регистрация',
-            portalTitle: 'Название'
         });
     });
 
     router.get('/seminars', function(req, res, next) {
         res.render('seminars', {
             title: 'Семинары',
-            portalTitle: 'Название'
         });
     });
 
     router.get('/resources', function(req, res, next) {
         res.render('resources', {
             title: 'Ресурсы',
-            portalTitle: 'Название'
         });
     });
 
